769f262594f61c271522f7177f17a6935a8e9b2b
ana.rajner@gmail.com <ana.rajner@gmail.com>
2018-03-13 09:24:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,6 +34,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 
 function removeElementFromBeginningOfArray(array) {
   var array;
-  array.slice(1);
+  return array.slice(1);
   return array;
 }
